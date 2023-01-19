1b367fef2444d3ba0fb128f54f1ed461081a8d9f
evgenius2049 <evgenius2049@tutanota.com>
2023-01-19 18:02:18 +0300

    регистрация модели EmailNotification в notifier_app.admin

--- notifier_app/admin.py
+++ notifier_app/admin.py
@@ -1,3 +1,55 @@
+# -*- coding: utf-8 -*-
+
+"""
+Use Python 3.10.0
+
+Панель администратора
+"""
+
 from django.contrib import admin
 
-# Register your models here.
+from notifier_app.models import EmailNotification
+
+
+class EmainNotficationAdmin(admin.ModelAdmin):
+    """Отображение модели EmailNotification в панели администратора
+    """
+
+    list_display = (
+        'id',
+        'sender',
+        'recipient',
+        'subject',
+        'notification_status',
+        'last_error_date',
+        'skipping',
+        'created_date',
+        'modified_date',
+        'version',
+    )
+
+    list_display_links = (
+        'id',
+    )
+
+    search_fields = (
+        'id',
+        'sender',
+        'recipient',
+        'subject',
+    )
+
+    list_editable = (
+        'notification_status',
+        'skipping',
+    )
+
+    list_filter = (
+        'recipient',
+        'subject',
+        'notification_status',
+        'skipping',
+    )
+
+
+admin.site.register(EmailNotification, EmainNotficationAdmin)
