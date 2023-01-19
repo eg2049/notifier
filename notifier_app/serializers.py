e036e5b8c83534ac739b3558ac1d45cfd9ba9dd1
evgenius2049 <evgenius2049@tutanota.com>
2023-01-19 18:09:50 +0300

    добавление сериализатора EmailNotificationSerializer в notifier_app.serializers

---
+++ notifier_app/serializers.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+"""
+Use Python 3.10.0
+
+Сериализаторы
+"""
+
+from rest_framework import serializers
+
+from notifier_app.models import EmailNotification
+
+
+class EmailNotificationSerializer(serializers.ModelSerializer):
+    """Сериализатор модели EmailNotification
+    """
+
+    pk = serializers.UUIDField(read_only=True)
+    notification_status = serializers.CharField(read_only=True)
+    sent_date = serializers.DateTimeField(read_only=True)
+    last_error_date = serializers.DateTimeField(read_only=True)
+    skipping = serializers.BooleanField(read_only=True)
+    created_date = serializers.DateTimeField(read_only=True)
+    modified_date = serializers.DateTimeField(read_only=True)
+    version = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        """Подключение модели которую необходимо сериализовывать
+        """
+
+        model = EmailNotification
+
+        fields = [
+            'pk',
+            'sender',
+            'recipient',
+            'subject',
+            'body',
+            'notification_status',
+            'sent_date',
+            'last_error_date',
+            'skipping',
+            'created_date',
+            'modified_date',
+            'version',
+        ]
+
+
+if __name__ == '__main__':
+    pass
