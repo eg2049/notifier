a9f43879faa10811985b9c6a3297b2a5cf992552
evgenius2049 <evgenius2049@tutanota.com>
2023-02-20 23:24:23 +0300

    перенос операции запуска демонов сервиса из manage.py в notifier_app.apps.py

--- notifier_app/apps.py
+++ notifier_app/apps.py
@@ -2,6 +2,24 @@ from django.apps import AppConfig
 
 
 class NotifierAppConfig(AppConfig):
+    """Конфигурация приложения
+    """
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'notifier_app'
     verbose_name = 'Notifier App'
+
+    def ready(self) -> None:
+        """Операции выполняемые после запуска "основного" приложения
+        """
+
+        from notifier_app.daemons.daemon_launcher import daemon_launcher
+
+        # запус демонов перед запуском "основного" приложения
+        daemon_launcher()
+
+        return super().ready()
+
+
+if __name__ == '__main__':
+    pass
